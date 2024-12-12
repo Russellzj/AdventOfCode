57e3fd80c4888957fd89002214330e6f5f80ada7
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-12 00:30:46 -0500

    Read data from file

--- 2024/Day01/src/Main.java
+++ 2024/Day01/src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File locations = new File("2024//Day01//Data//Locations.txt");
+        //Following holds location data from file
+        List<Integer> locationsA  = new ArrayList<>();
+        List<Integer> locationsB = new ArrayList<>();
+        try(Scanner sc = new Scanner(locations)) {
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] twoLocations = line.split("   ");
+                locationsA.add(Integer.parseInt(twoLocations[0]));
+                locationsB.add(Integer.parseInt(twoLocations[1]));
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
