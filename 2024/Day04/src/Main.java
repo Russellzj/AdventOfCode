1c5045b9ddfd4611372d72181a705fc17231fd71
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-13 18:51:11 -0500

    Complete Task 2

--- 2024/Day04/src/Main.java
+++ 2024/Day04/src/Main.java
@@ -17,11 +17,40 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        
         System.out.println(findWord(wordSearch, "XMAS"));
+        System.out.println(findShape(wordSearch, "MAS"));
+    }
+    //PART 02
+    public static int findShape(List<char[]> wordSearch, String word) {
+        int count = 0;
+        String wordR = "";
+        for (char c : word.toCharArray()) {
+            wordR = c + wordR;
+        }
+        int[][]coordsA = {{0, 0}, {1, 1}, {2, 2}};
+        int[][]coordsB = {{0, 2}, {1, 1}, {2, 0}};
+        for (int i = 0; i < wordSearch.size(); i++) {
+            for (int j = 0; j < wordSearch.get(i).length; j++) {
+                if (i + word.length() - 1 < wordSearch.get(i).length && j + word.length() - 1 < wordSearch.get(i + 2).length) {
+                    String wordCheckA = "";
+                    for (int[] coords : coordsA) {
+                        wordCheckA += String.valueOf(wordSearch.get(i + coords[0])[coords[1] + j]);
+                    }
+                    String wordCheckB = "";
+                    for (int[] coords : coordsB) {
+                        wordCheckB += String.valueOf(wordSearch.get(i + coords[0])[coords[1] + j]);
+                    }
+                    if (wordCheckA.equals(word) || wordCheckA.equals(wordR))
+                        if (wordCheckB.equals(word) || wordCheckB.equals(wordR)) {
+                            count++;
+                        }
+                }
+            }
+        }
+        return count;
     }
 
+    //PART 01
     public static int findWord(List<char[]> wordSearch, String word) {
         char[] wordToFind= word.toCharArray();
         int wordCount = 0;
@@ -50,17 +79,12 @@ public class Main {
         return wordCount;
     }
 
-
-    
-
-
     public static boolean isWordInDirection(List<char[]> wordSearch, int x, int y, char[] word,
                                             int letter, int dx, int dy) {
         int nextX = x + dx ;
         if (nextX < wordSearch.size() && nextX >= 0) {
             int nextY = y + dy;
             if (nextY < wordSearch.get(nextX).length && nextY >= 0) {
-                char test = wordSearch.get(nextX)[nextY];
                 if(wordSearch.get(nextX)[nextY] == word[letter]) {
                     if (letter >= word.length - 1) {
                         return true;
