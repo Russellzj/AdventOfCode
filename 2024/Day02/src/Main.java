853d1a8f06d59d262e00dc308b819f00a2e8f284
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-12 22:01:59 -0500

    Reads in Data

--- 2024/Day02/src/Main.java
+++ 2024/Day02/src/Main.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File locations = new File("2024//Day02//Data//RedReactor");
+        List<List<Integer>> redReactor = new ArrayList<>();
+        try (Scanner sc = new Scanner(locations)){
+            while (sc.hasNextLine()) {
+                List<Integer> report = new ArrayList<>();
+                String[] values = sc.nextLine().split(" ");
+                for (String value : values) {
+                    report.add(Integer.parseInt(value));
+                }
+                redReactor.add(report);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(redReactor.size());
     }
 }
