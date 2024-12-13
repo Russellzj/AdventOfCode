945bf223f7e64aa590f7f101d4ff289cf6291d6f
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-13 12:13:30 -0500

    Import data and add information to string

--- 2024/Day03/src/Main.java
+++ 2024/Day03/src/Main.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File corruptData = new File("2024//Day03//Data//input");
+        StringBuilder corrupt = new StringBuilder();
+        try (Scanner sc = new Scanner(corruptData)) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                corrupt.append(line).append(" ");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(corrupt);
     }
 }
