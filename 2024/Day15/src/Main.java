639ee5f6a860b415490cf6bd3897573157eb82d5
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-16 21:12:58 -0500

    Import Data to Code

--- 2024/Day15/src/Main.java
+++ 2024/Day15/src/Main.java
@@ -1,5 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File mapAndMovements = new File("2024/Day15/Data/TestMapAndMovements");
+        List<List<Character>> map = new ArrayList<>();
+        List<Character> movements = new ArrayList<>();
+        try (Scanner sc = new Scanner(mapAndMovements)) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.isEmpty()) {
+                    break;
+                }
+                List<Character> mapRow = new ArrayList<>();
+                for (char c : line.toCharArray()) {
+                    mapRow.add(c);
+                }
+                map.add(mapRow);
+            }
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                for (char c : line.toCharArray()) {
+                    movements.add(c);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("END");
     }
 }
