4c1f8b97da46640141d641602609f23486e31a1c
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-19 20:13:34 -0500

    Import Data to code

--- 2024/Day19/src/Main.java
+++ 2024/Day19/src/Main.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file = new File("2024/Day19/Data/TestTowelDesigns");
+        Set<String> towels = new HashSet<>();
+        List<String> patterns = new ArrayList<>();
+        try (Scanner sc = new Scanner(file);) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.isEmpty()) break;
+                for (String towel : line.split(", ")) {
+                    towels.add(towel);
+                }
+            }
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                patterns.add(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("END");
     }
 }
