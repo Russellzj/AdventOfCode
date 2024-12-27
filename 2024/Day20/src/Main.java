3ca6483b1489933aead8567f575d4dedab18815b
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-27 16:36:21 -0500

    Basic File to List

--- 2024/Day20/src/Main.java
+++ 2024/Day20/src/Main.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<List<Character>> race = new ArrayList<>();
+        File file = new File("2024//Day20//Data//TestRaceTrack");
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNextLine()) {
+                char[] line = sc.nextLine().toCharArray();
+                List<Character> raceLine = new ArrayList<>();
+                for (char track : line) {
+                    raceLine.add(track);
+                }
+                race.add(raceLine);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("End");
     }
 }
